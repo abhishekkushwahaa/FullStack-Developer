beb503983a01ca422d3cbd31c55cae34dd4adb1a
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2023-12-22 08:15:14 +0530

    NodeJs

---
+++ Backend/NodeJs/modules/index.js
@@ -0,0 +1,4 @@
+const math = require('./math');
+console.log(math);
+console.log(math.addFn(1,2));
+console.log(math.subFn(1,2));
---
+++ Backend/NodeJs/modules/math.js
@@ -0,0 +1,15 @@
+// function add(a,b){
+//     return a+b;
+// }
+
+// function sub(a,b){
+//     return a-b;
+// }
+
+// module.exports = {
+//     addFn : add,
+//     subFn : sub
+// };
+
+exports.addFn = (a,b) => a+b;
+exports.subFn = (a,b) => a-b;
