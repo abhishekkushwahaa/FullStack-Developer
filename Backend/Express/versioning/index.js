f7a5b4dcec4fd56a829c71ac65c4ba90c86c3566
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-01-08 12:29:39 +0530

    Express Versioning

---
+++ Backend/Express/versioning/index.js
@@ -0,0 +1,19 @@
+// Versioning
+
+// 4.18.1
+
+/*
+first part -> 4 // major version
+second part -> 18 // minor version
+third part -> 1 // patch version
+
+major version -> major and breaking changes (Acceptable but not recommended)
+minor version -> new features and bug fixes (Recommended)
+patch version -> bug fixes and minor fixes (Optional)
+*/
+
+// Command to install a specific version of express (Website: https://www.npmjs.com/package/express)
+// npm install express@4.17.2
+
+// Command to install the latest version of express
+// npm install express@latest
