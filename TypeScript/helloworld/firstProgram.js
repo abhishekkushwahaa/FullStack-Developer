7dea72c094e0addd968cc2b94c2e3ef9f6d83cae
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-01-11 09:48:22 +0530

    typescript

---
+++ TypeScript/helloworld/firstProgram.js
@@ -0,0 +1,4 @@
+var age = 19;
+var firstName = 'Abhi';
+var lastName = 'Maurya';
+console.log("My name is ".concat(firstName, " ").concat(lastName, " and I am ").concat(age, " years old."));
---
+++ TypeScript/helloworld/firstProgram.ts
@@ -0,0 +1,4 @@
+let age: number = 19;
+let firstName: string = 'Abhi';
+let lastName: string = 'Maurya';
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old.`);
